c12d4a60bfd59856ea6df09b21e1898e205bad46
Anıl Kuşçu <63120231+anilkusc@users.noreply.github.com>
2020-09-28 16:05:31 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+apt-get update && apt-get install jq curl -y
+TARGET_POD=$1
 TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
+NAMESPACE=$2
+if [[ $NAMESPACE == "" ]]; then
 NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
-if [ $DEBUG == "on" ];then
-sleep 100000000
-else
-curl -X PATCH -H  "Authorization: Bearer $TOKEN" -H 'Content-Type: application/strategic-merge-patch+json' --data '{"spec": {"replicas":0}}' 'https://kubernetes.default.svc.cluster.local/apis/apps/v1/namespaces/'$NAMESPACE'/deployments/'$1 --insecure
-sleep 10s
-curl -X PATCH -H  "Authorization: Bearer $TOKEN" -H 'Content-Type: application/strategic-merge-patch+json' --data '{"spec": {"replicas":1}}' 'https://kubernetes.default.svc.cluster.local/apis/apps/v1/namespaces/'$NAMESPACE'/deployments/'$1 --insecure
-
 fi
+POD_COUNT=$(curl -s -H "Authorization: Bearer $TOKEN" https://kubernetes/api/v1/namespaces/$NAMESPACE/pods/ --insecure | jq '.items | length')
+for (( c=0; c<=$POD_COUNT; c++ ))
+do  
+   POD_NAME=$(curl -H "Authorization: Bearer $TOKEN" https://kubernetes/api/v1/namespaces/$NAMESPACE/pods/ --insecure | jq .items | jq .[$i] | jq .metadata | jq .name)
+   if [[ $POD_NAME == *$TARGET_POD* ]]; then
+   curl https://kubernetes/api/v1/namespaces/$NAMESPACE/pods/$POD_NAME -X DELETE -k -H "Authorization: Bearer $TOKEN"
+   fi
+done
