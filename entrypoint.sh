ab1f3fd4ca9a7fa15a0c9a8f8c4457b8d629d4cf
Anıl Kuşçu <63120231+anilkusc@users.noreply.github.com>
2020-09-29 11:59:16 +0300

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
-apt-get update && apt-get install jq curl -y
-TARGET_POD=$1
-TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
-NAMESPACE=$2
-if [[ $NAMESPACE == "" ]]; then
-NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
-fi
-POD_COUNT=$(curl -s -H "Authorization: Bearer $TOKEN" https://kubernetes/api/v1/namespaces/$NAMESPACE/pods/ --insecure | jq '.items | length')
-for (( c=0; c<=$POD_COUNT; c++ ))
-do  
-   POD_NAME=$(curl -H "Authorization: Bearer $TOKEN" https://kubernetes/api/v1/namespaces/$NAMESPACE/pods/ --insecure | jq .items | jq .[$i] | jq .metadata | jq .name)
-   if [[ $POD_NAME == *$TARGET_POD* ]]; then
-   curl https://kubernetes/api/v1/namespaces/$NAMESPACE/pods/$POD_NAME -X DELETE -k -H "Authorization: Bearer $TOKEN"
-   fi
-done
+    #!/bin/bash
+    apt-get update && apt-get install jq curl -y
+    TARGET_POD=$1
+    TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
+    NAMESPACE=$2
+    if [[ $NAMESPACE == "" ]]; then
+    NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
+    fi
+    KUBERNETES=https://kubernetes.default.svc.cluster.local/api/v1/namespaces/$NAMESPACE/pods
+    POD_COUNT=$(curl -s -H "Authorization: Bearer $TOKEN" $KUBERNETES --insecure | jq '.items | length')
+    for (( c=0; c<=$POD_COUNT ; c++ ))
+    do
+       POD_NAME=$(curl -s -H "Authorization: Bearer $TOKEN" $KUBERNETES --insecure |jq .items | jq .[$c] | jq .metadata | jq -r .name)
+       if [[ $POD_NAME == *$TARGET_POD* ]]; then
+       curl -s $KUBERNETES/$POD_NAME -X DELETE -k -H "Authorization: Bearer $TOKEN"
+       fi
+    done
